d29dbb3d215c99117fdc5b3a3796d4f3f5307216
Bhanuteja716 <tejab0985@gmail.com>
2025-07-08 12:20:17 +0530

    Add files via upload

--- Lab1.sql
+++ Lab1.sql
@@ -6,3 +6,14 @@ insert into student(name,age,address) values ("Ram",14,"Ayodhya");
 insert into student(name,age,address) values("Shiva",15,"Kailas");
 insert into student(name,age,address) values("Krishna",18,"Dwaraka");
 select * from student;
+create table account1(accname varchar(15),accnum varchar(10),accbal int(10));
+desc account1
+insert into account1(accname,accnum,accbal) values("Savings Account","SBN24210",500000);
+insert into account1(accname,accnum,accbal) values("Deposit Account","ICICI2007",200000);
+select * from account1;
+create table animal(name varchar(10), age int(3), type varchar(15));
+desc animal
+insert into animal(name,age,type) values("Lion",24,"Carnivorus");
+insert into animal(name,age,type) values("Elephant",52,"Herbivore");
+insert into animal(name,age,type) values("Human",78,"Omnivore");
+select * from animal;
